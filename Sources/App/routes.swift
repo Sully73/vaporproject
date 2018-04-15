285d7e3871811773305de7aaa578bfdda36dedcc
Cory Sullivan <cory.sullivan@redspace.com>
2018-04-15 19:46:06 -0300

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,13 +6,14 @@ import Vapor
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get("hello") { _ in
+        "Hello, world!"
     }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        try req.content.decode(Acronym.self)
+            .flatMap(to: Acronym.self, { acronym in
+                acronym.save(on: req)
+            })
+    }
 }
