3699c79705967254ee3d9e92cd1a0d1faaae149f
Cory Sullivan <cory.sullivan@redspace.com>
2018-04-21 20:41:52 -0300

    CRUD operations

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,6 @@
 import Routing
 import Vapor
+import Fluent
 
 /// Register your application's routes here.
 ///
@@ -15,5 +16,28 @@ public func routes(_ router: Router) throws {
             .flatMap(to: Acronym.self, { acronym in
                 acronym.save(on: req)
             })
+    }
+    
+    router.get("api", "acronyms") { req -> Future<[Acronym]> in
+        return Acronym.query(on: req).all()
+    }
+    
+    router.get("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+        return try req.parameter(Acronym.self)
+    }
+    
+    router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+        return try flatMap(to: Acronym.self, req.parameter(Acronym.self), req.content.decode(Acronym.self), { (acronym, updatedAcronym) in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req)
+        })
+    }
+    
+    router.get("api", "acronyms", "search") { req -> Future<[Acronym]> in
+        guard let searchTerm = req.query[String.self, at: "term"] else {
+            throw Abort(.badRequest)
+        }
+        return try Acronym.query(on: req).filter(\.short == searchTerm).all()
     }
 }
